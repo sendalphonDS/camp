79411671d08bf21d4a89e156dde31ecc2fbc95f8
unknown <yor-el-20@hotmail.comm>
2020-08-19 23:57:09 -0500

    camp

---
+++ js/vendor/main.js
@@ -0,0 +1,19 @@
+ (function() {
+     "use strict";
+
+     document.addEventListener('DOMContentLoaded', function() {
+
+         //campos datos usuario
+
+
+
+         var nombre = document.getElementById('nombre');
+         console.log('nombre');
+         var apellido = document.getElementById('apellido');
+         var email = document.getElementById('email');
+
+         //campos pases
+
+
+     }); //dom content loaded
+ })();
